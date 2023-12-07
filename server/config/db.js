d54038cabd75d18e680579b5398cc601ed1b80ce
Omied <143910865+omiedmuhebi@users.noreply.github.com>
2023-12-06 20:16:35 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,5 +5,5 @@ module.exports =
   // This is the connection string to the MongoDB database
   // The format is: mongodb://[hostname]/[database-name]
   // Here, '127.0.0.1' refers to the localhost, and 'shoppingCart' is the name of the database
-  "URI": "mongodb+srv://omiedmuhebi:WJGpqmhu2jakvbVu@finalproject.4kr9o2p.mongodb.net/shoppingCart"
+  "URI": "mongodb+srv://omiedmuhebi:WJGpqmhu2jakvbVu@finalproject.4kr9o2p.mongodb.net"
 }
